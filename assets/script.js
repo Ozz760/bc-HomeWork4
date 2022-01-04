25732c63305d0416fd293ce1ef60b3de54a316e1
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-03 21:48:59 -0800

    Psuedocode

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// Make Var for all of the elements you want to use for the game. 
+// Make an object for all of the question the quiz has. 
+// Make an object for all of the right answer to the game 
+// Have IF statements for all of the correct answers to each question. 
+// Make each question to the game have their own IF statements for their correct answers. 
+// Set up timer 
+// Have the timer set back 10 seconds everytime the user gets the answer wrong. 
