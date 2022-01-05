1b3e8b2ba029f54d6a878eb2848c1e77be59fb3c
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-04 18:06:28 -0800

    update

--- assets/script.js
+++ assets/script.js
@@ -4,4 +4,9 @@
 // Have IF statements for all of the correct answers to each question. 
 // Make each question to the game have their own IF statements for their correct answers. 
 // Set up timer 
-// Have the timer set back 10 seconds everytime the user gets the answer wrong. 
+// Have the timer set back 10 seconds everytime the user gets the answer wrong.
+// Page where the user can put their inistals to keep track of their score.
+// Make a button to have the user start the quiz again. 
+// Make a button to give the user the option to clear all of the scores. 
+// 
+
