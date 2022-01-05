85c831a6be27cb71db187805f16e947f5fbb8419
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-04 22:18:56 -0800

    JS

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var questions = document.querySelector(".box"); 
+var container = document.querySelector(".container")
+var startButton = document.querySelector(".start-button")
+
+// Make a timer for the quiz 
+// Make answers to the quiz 
+// Make a highscore page 
+// Make buttons for the user to put in their inisials 
+// Make a button for the game to start
