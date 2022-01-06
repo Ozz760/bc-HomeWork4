99f47ea0907a0d8ccf0fc00f1ee266258f06cdfa
Oscar Silva <oscarsilva2356@gmail.com>
2022-01-05 17:57:09 -0800

    js qs

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,30 @@ var questions = document.querySelector(".box");
 var container = document.querySelector(".container")
 var startButton = document.querySelector(".start-button")
 var timeEl = document.querySelector(".time");
-var secondsLeft = 60; 
+
+var secondsLeft = 75; 
+
+var question = [
+    {
+        question : "What does HTML stand for?", 
+        choices : ["Hypertext Markup Language", "Hello, Tom Mark Larry", "How is That Monkey so Large?", "None of the above"],
+        answer: "Hypertext Markup Language", 
+    },
+    {
+        question : "Which programming language is used by most websites today", 
+        choices : ["PHP", "Java", "JavaScript", "C"], 
+        answer : "JavaScript"
+    }
+];
+
+
+
+
+
+function startGame() {
+    setTimer();
+    displayQuestions();
+}
 
 
 
